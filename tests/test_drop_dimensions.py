f3369fc12266c30efae4ae99c276b8763333bd40
Eelco van Vliet <eelcovv@gmail.com>
2024-10-29 08:49:30 +0100

    added class around test

--- tests/test_drop_dimensions.py
+++ tests/test_drop_dimensions.py
@@ -20,10 +20,7 @@ GROUP_BY = {"gk6sbi2": {"dimensions": ["gk", "sbi"]}, "drop_dimensions": True}
 # - Test voor een situatie met een filter.
 
 
-def test_drop_dimensions_no_drop():
-    """
-    Test voor var_type 'float' met imputatiemethode 'mean'
-    """
+class TestDropDimensions:
     records_df = pd.DataFrame(
         [
             [1, 1, "A", "10", 1.2],
@@ -45,70 +42,53 @@ def test_drop_dimensions_no_drop():
         }
     }
 
-    # Init ImputeGaps
-    impute_gaps = ImputeGaps(
-        variables=variables,
-        imputation_methods=IMPUTATION_METHODS,
-        index_key=ID_KEY,
-        seed=SET_SEED,
-    )
+    def test_drop_dimensions_no_drop(self):
+        """
+        Test for var_type 'float' with imputation method 'mean'
+        """
 
-    new_records = impute_gaps.impute_gaps(
-        records_df=records_df, group_by=["gk", "sbi"], drop_dimensions=False
-    )
+        # Init ImputeGaps
+        impute_gaps = ImputeGaps(
+            variables=self.variables,
+            imputation_methods=IMPUTATION_METHODS,
+            index_key=ID_KEY,
+            seed=SET_SEED,
+        )
 
-    # Expected
-    expected_telewerkers = pd.Series(
-        [1.2, 2.3, 3.4, 4.5, 2.85, None, None], copy=False, name="telewerkers"
-    )
-    new_expected_telewerkers = new_records["telewerkers"]
+        new_records = impute_gaps.impute_gaps(
+            records_df=self.records_df, group_by=["gk", "sbi"], drop_dimensions=False
+        )
 
-    # Test uitvoeren
-    pd.testing.assert_series_equal(new_expected_telewerkers, expected_telewerkers)
+        # Expected
+        expected_telewerkers = pd.Series(
+            [1.2, 2.3, 3.4, 4.5, 2.85, None, None], copy=False, name="telewerkers"
+        )
+        new_expected_telewerkers = new_records["telewerkers"]
 
+        # Test uitvoeren
+        pd.testing.assert_series_equal(new_expected_telewerkers, expected_telewerkers)
 
-def test_drop_dimensions_drop():
-    """
-    Test voor var_type 'float' met imputatiemethode 'mean'
-    """
-    records_df = pd.DataFrame(
-        [
-            [1, 1, "A", "10", 1.2],
-            [2, 1, "A", "10", 2.3],
-            [3, 2, "A", "10", 3.4],
-            [4, 2, "A", "10", 4.5],
-            # Eentje leeg in stratum ['sbi', 'gk']:
-            [5, 2, "A", "10", None],
-            # Alles leeg in stratum ['sbi', 'gk'], maar niet in ['gk']:
-            [6, 1, "C", "10", None],
-            # Alles leeg in stratum ['sbi', 'gk'], maar ook in ['gk']:
-            [7, 1, "C", "20", None],
-        ],
-        columns=["be_id", "internet", "sbi", "gk", "telewerkers"],
-    )
-    variables = {
-        "telewerkers": {
-            "type": "float",
-        }
-    }
+    def test_drop_dimensions_drop(self):
+        """
+        Test for var_type 'float' with imputation method 'mean'
+        """
+        # Init ImputeGaps
+        impute_gaps = ImputeGaps(
+            variables=self.variables,
+            imputation_methods=IMPUTATION_METHODS,
+            index_key=ID_KEY,
+            seed=SET_SEED,
+        )
 
-    # Init ImputeGaps
-    impute_gaps = ImputeGaps(
-        variables=variables,
-        imputation_methods=IMPUTATION_METHODS,
-        index_key=ID_KEY,
-        seed=SET_SEED,
-    )
+        new_records = impute_gaps.impute_gaps(
+            records_df=self.records_df, group_by=["gk", "sbi"], drop_dimensions=True
+        )
 
-    new_records = impute_gaps.impute_gaps(
-        records_df=records_df, group_by=["gk", "sbi"], drop_dimensions=True
-    )
+        # Expected
+        expected_telewerkers = pd.Series(
+            [1.2, 2.3, 3.4, 4.5, 2.85, 2.85, 2.85], copy=False, name="telewerkers"
+        )
+        new_expected_telewerkers = new_records["telewerkers"]
 
-    # Expected
-    expected_telewerkers = pd.Series(
-        [1.2, 2.3, 3.4, 4.5, 2.85, 2.85, 2.85], copy=False, name="telewerkers"
-    )
-    new_expected_telewerkers = new_records["telewerkers"]
-
-    # Test uitvoeren
-    pd.testing.assert_series_equal(new_expected_telewerkers, expected_telewerkers)
+        # Test uitvoeren
+        pd.testing.assert_series_equal(new_expected_telewerkers, expected_telewerkers)
